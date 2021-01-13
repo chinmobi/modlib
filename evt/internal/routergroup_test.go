5bc300f47904ee5948dfafbee8338e0561b41d56
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-12 22:09:19 -0500

    Add test for routergroup

---
+++ evt/internal/routergroup_test.go
@@ -0,0 +1,28 @@
+// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouterGroupBasic(t *testing.T) {
+	router := newEngine()
+	group := router.Group("/hola", func(c *Context) {})
+	group.Use(func(c *Context) {})
+
+	assert.Len(t, group.Handlers, 2)
+	assert.Equal(t, "/hola", group.BasePath())
+	assert.Equal(t, router, group.engine)
+
+	group2 := group.Group("manu")
+	group2.Use(func(c *Context) {}, func(c *Context) {})
+
+	assert.Len(t, group2.Handlers, 4)
+	assert.Equal(t, "/hola/manu", group2.BasePath())
+	assert.Equal(t, router, group2.engine)
+}
