383c19f15bdf8d16ccff9a84a8b406c8fa972965
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-12 22:38:18 -0500

    Import event multicaster used for internal

---
+++ evt/internal/evt_multicaster.go
@@ -0,0 +1,11 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"github.com/chinmobi/modlib/evt/event"
+)
+
+type EventMulticaster = event.Multicaster
