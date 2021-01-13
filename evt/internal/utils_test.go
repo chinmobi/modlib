2e7de90b88917e6f0af6291542dad016fa81ff91
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-12 21:33:02 -0500

    Add test for route helper functions

---
+++ evt/internal/utils_test.go
@@ -0,0 +1,30 @@
+// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLastChar(t *testing.T) {
+	assert.Equal(t, uint8('a'), lastChar("hola"))
+	assert.Equal(t, uint8('s'), lastChar("adios"))
+	assert.Panics(t, func() { lastChar("") })
+}
+
+func TestJoinPaths(t *testing.T) {
+	assert.Equal(t, "", joinPaths("", ""))
+	assert.Equal(t, "/", joinPaths("", "/"))
+	assert.Equal(t, "/a", joinPaths("/a", ""))
+	assert.Equal(t, "/a/", joinPaths("/a/", ""))
+	assert.Equal(t, "/a/", joinPaths("/a/", "/"))
+	assert.Equal(t, "/a/", joinPaths("/a", "/"))
+	assert.Equal(t, "/a/hola", joinPaths("/a", "/hola"))
+	assert.Equal(t, "/a/hola", joinPaths("/a/", "/hola"))
+	assert.Equal(t, "/a/hola/", joinPaths("/a/", "/hola/"))
+	assert.Equal(t, "/a/hola/", joinPaths("/a/", "/hola//"))
+}
