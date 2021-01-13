892e795c5d6c47d0f9fde09c57e1f39d21a7aad2
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-12 22:21:51 -0500

    Fix source code copyright

--- evt/internal/engine.go
+++ evt/internal/engine.go
@@ -1,3 +1,7 @@
+// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style license that can be found
+// at https://github.com/gin-gonic/gin/blob/master/LICENSE
+
 // Copyright 2020 Zhaoping Yu.  All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
