a99e0effd8a3bc59177e750af889b83e61ed2c4d
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-12 21:10:04 -0500

    Import the event concepts used for internal

---
+++ evt/internal/evt_event.go
@@ -0,0 +1,23 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"github.com/chinmobi/modlib/evt/event"
+)
+
+type EventPayload = event.Payload
+
+// --- EventHandler ---
+
+type EventHandler = event.Handler
+
+// --- Event ---
+
+type Event = event.Event
+
+func NewEvent() *Event {
+	return event.NewEvent()
+}
