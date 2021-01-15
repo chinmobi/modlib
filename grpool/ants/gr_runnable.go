9eff2dc56a9541e54330448ab3b9696e0dde6524
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-15 10:28:58 -0500

    Import gr.Runnable used for ants

---
+++ grpool/ants/gr_runnable.go
@@ -0,0 +1,11 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package ants
+
+import (
+	"github.com/chinmobi/modlib/grpool/gr"
+)
+
+type Runnable = gr.Runnable
