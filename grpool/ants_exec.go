44341f9c339f3c6547b277001fdc02a06ec1adaa
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-15 13:32:20 -0500

    Implement ExecutorService with ants Pool

---
+++ grpool/ants_exec.go
@@ -0,0 +1,31 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package grpool
+
+import (
+	"github.com/chinmobi/modlib/grpool/ants"
+	"github.com/chinmobi/modlib/grpool/gr"
+)
+
+type antsExecutor struct {
+	pool *ants.Pool
+}
+
+func (exec *antsExecutor) Execute(task gr.Runnable) error {
+	return exec.pool.Submit(task)
+}
+
+func (exec *antsExecutor) Shutdown() {
+	exec.pool.Release()
+}
+
+func NewAntsExecutor(poolSize int, opts *ants.Options) (gr.ExecutorService, error) {
+	pool, err := ants.NewPool(poolSize, ants.WithOptions(opts))
+	if err != nil {
+		return nil, err
+	}
+
+	return &antsExecutor{pool: pool,}, nil
+}
