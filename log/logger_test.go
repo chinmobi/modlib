870b82324f840f5772caa986005023b752b857d7
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-16 11:32:47 -0500

    Add logger tests

---
+++ log/logger_test.go
@@ -0,0 +1,51 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package log_test
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/chinmobi/modlib/log"
+)
+
+func setUp(t *testing.T) {
+	config := log.DefaultConfig()
+
+	tempDir, err := ioutil.TempDir("", "*-logs")
+	if err == nil {
+		config.File.Enabled = true
+		config.File.Filename = filepath.Join(tempDir, "test.log")
+	} else {
+		t.Logf("%+v", err)
+	}
+
+	log.SetUpLogger(config)
+}
+
+func TestLoggerUsage(t *testing.T) {
+	defer func() {
+		if p := recover(); p != nil {
+			// discard
+		}
+	}()
+
+	setUp(t)
+
+	log.Debug("A debug msg")
+	log.Debugf("A %s msg", "debug")
+
+	log.Info("A info msg")
+	log.Infof("A %s msg", "info")
+
+	log.Warn("A warn msg")
+	log.Warnf("A %s msg", "warn")
+
+	log.Error("A error msg")
+	log.Errorf("A %s msg", "error")
+
+	log.Panic("A panic msg")
+}
